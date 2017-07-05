8d908bacb29ec99fff490f2fe08d518e84ec2572
hazemsalah <hazemsalah31296@hotmail.com>
2017-07-05 14:02:52 +0300

    Change password and searching a user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,19 +1,37 @@
 <?php
 namespace App\Http\Controllers;
+use App\Mail\TaskInvitation;
+use App\Task;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use JWTAuth;
 use App\User;
+use App\Mail\Welcome;
 use JWTAuthException;
+
+/**
+ * Class UserController
+ * @package App\Http\Controllers
+ */
 class UserController extends Controller
 {
+    /**
+     * @var User
+     */
     private $user;
+    /**
+     * UserController constructor.
+     * @param User $user
+     */
     public function __construct(User $user){
         $this->user = $user;
-        $this->middleware('jwt.auth')->except(['login','register']);
     }
-
+    /**
+     * registers a new user
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function register(Request $request){
         $this->validate(request(),[
             'name' => 'required',
@@ -25,14 +43,17 @@ class UserController extends Controller
             'email' => $request->get('email'),
             'password' => bcrypt($request->get('password'))
         ]);
-
+        \Mail::to($user)->send(new Welcome);
        return response()->json(['status'=>true,'message'=>'User created successfully','data'=>$user]);
     }
-
+    /**
+     * logs in with a user
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(Request $request){
         $credentials = $request->only('email', 'password');
         $token = null;
-
         try {
             if (!$token = JWTAuth::attempt($credentials)) {
                 return response()->json(['invalid_email_or_password'], 422);
@@ -40,22 +61,65 @@ class UserController extends Controller
         } catch (JWTAuthException $e) {
             return response()->json(['failed_to_create_token'], 500);
         }
-
-
         return response()->json(compact('token'));
     }
+    /**
+     * gets the logged in user
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function getAuthUser(Request $request){
         $user = JWTAuth::toUser($request->token);
         return response()->json(['result' => $user]);
     }
+    /**
+     * deletes a user
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy(){
         JWTAuth::invalidate(JWTAuth::getToken());
         return response()->json('You are successfully logged out');
     }
+    /**
+     * changes the user's password
+     * @request('new_password')
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function changePassword(){
         $user=User::find(auth()->id());
         $user->password=bcrypt(request('new_password'));
         $user->save();
         return response()->json('Password Updated Successfully');
+    }
+    /**
+     * invites another user to view private task
+     * @request('task_id')
+     * @request('user_id')
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function invite(){
+        $this->validate(request(),[
+            'task_id' => 'required',
+            'user_id'=> 'required'
+        ]);
+        $task=Task::find(request('task_id'));
+
+        $user=User::find(request('user_id'));
+        if (\Gate::denies('taskOwner', $task)) {
+            return response()->json("Task doesn't Belong To you");
+        }
+        \Mail::to($user)->send(new TaskInvitation($user,$task));
+        return response()->json("Invitation Mail Sent Successfully");
+    }
+    /**
+     * searches a certain user by name
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function searchUser(){
+        $this->validate(request(),[
+            'name' => 'required'
+        ]);
+        $users =User::where('name',request('name'))->orWhere('name', 'like', '%' . request('name') . '%')->get();
+        return response()->json($users);
     }
 }
